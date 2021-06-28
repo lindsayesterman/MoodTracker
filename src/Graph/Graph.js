0ccb7eee4e77361056d17163ea740fb2f28f0160
Lindsay Esterman <lindsayesterman@gmail.com>
2021-06-28 13:38:40 -0400

    flexbox update mobile

--- src/Graph/Graph.js
+++ src/Graph/Graph.js
@@ -14,24 +14,26 @@ export default class Graph extends Component {
         <div className="graph">
           <div className="statContainer">
             <h1>Your mood over time with Shimmer:)</h1>
-            <div className="statBox">
-              <h3>Your most common stats</h3>
-              <div className="boxInsideStatBox">
-                <p>
-                  <b>Mood: </b> Happy
-                </p>
-                <br />
-                <p>
-                  <b>Tags: </b> School, Work, Food
-                </p>
+            {/* <div className="boxAndButtons"> */}
+              <div className="statBox">
+                <h3>Your most common stats</h3>
+                <div className="boxInsideStatBox">
+                  <p>
+                    <b>Mood: </b> Happy
+                  </p>
+                  <br />
+                  <p>
+                    <b>Tags: </b> School, Work, Food
+                  </p>
+                </div>
+              </div>
+              <div className="buttonContainer">
+                <button className="week">Week</button>
+                <button className="month">Month</button>
+                <button className="year">Year</button>
               </div>
             </div>
-            <div className="buttonContainer">
-              <button className="week">Week</button>
-              <button className="month">Month</button>
-              <button className="year">Year</button>
-            </div>
-          </div>
+          {/* </div> */}
           <motion.img
             alt="fake graph"
             initial="outRight"
