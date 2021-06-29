e1fc65ad94117e2ce1f6b618cb61cf9edb8923cd
Lindsay Esterman <lindsayesterman@gmail.com>
2021-06-29 12:38:22 -0400

    change graph w/ time range

--- src/Graph/Graph.js
+++ src/Graph/Graph.js
@@ -13,54 +13,86 @@ export default class Graph extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      chartData: {},
-      timeRange: "",
+      graphData: {},
+      timeRange: "week",
     };
   }
 
   componentDidMount() {
-    this.getChartData();
+    this.getGraphData();
   }
 
   handleTimeRangeClicked = (e) => {
     this.setState({
       timeRange: e.target.value,
     });
-    console.log(e.target.value);
+    console.log("state" + this.state.timeRange);
+    this.getGraphData();
   };
 
-  getChartData = () => {
-    // const myImage = new Image();
-    // myImage.src = { f1 };
+  addGraphGradient = () => {
     var ctx = document.getElementById("lineChart").getContext("2d");
     var gradient = ctx.createLinearGradient(0, 0, 0, 400);
-    gradient.addColorStop(0, "rgba(255, 241, 169, .8)");
-    gradient.addColorStop(1, "rgba(255, 241, 169, 0) 0)");
+    gradient.addColorStop(0, "rgba(255, 240, 164, 1)");
+    gradient.addColorStop(1, "rgba(201, 236, 255, 0) 0)");
+    return gradient;
+  };
 
-    const data = {
-      labels: [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ],
-      datasets: [
-        {
-          label: "# of Votes",
-          data: [1, 3, 3, 4, 3, 5, 4, 5, 3, 5, 3, 4, 2, 3, 4],
-          fill: true,
-          backgroundColor: gradient,
-          borderColor: "black",
-          borderWidth: 1,
-          pointBackgroundColor: "#7FBEF9",
-          pointBorderColor: "#7FBEF9",
-          pointRadius: 5,
-        },
-      ],
-    };
+  whichData = () => {
+    if (this.state.timeRange === "week") {
+      const data = {
+        labels: [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+        ],
+        datasets: {
+          data: [1, 3, 3, 4, 3, 5, 4],
+        }
+      };
+      return { data };
+    } else if (this.state.timeRange === "month") {
+      const data = {
+        labels: [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+        ],
+        datasets: {
+          data: [1, 3, 3, 2, 3, 2, 4, 5, 3, 5, 3, 4, 5, 3, 4,4,5,3,4,5,4,5,4,1,5,4],
+        }
+      };
+      return { data };
+    } else if (this.state.timeRange === "year") {
+      const data = {
+        labels: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+        datasets: {
+          data: [1, 3, 3, 2, 3, 2, 4, 5, 3, 5, 3, 4],
+        }
+      };
+      return { data };
+    }
+  };
+
+  getGraphData = () => {
+    const dataLabels = this.whichData().data.labels;
+    const numericalData = this.whichData().data.datasets.data
 
     const options = {
       plugins: { legend: { display: false } },
@@ -89,8 +121,23 @@ export default class Graph extends Component {
     };
 
     this.setState({
-      chartData: {
-        data: data,
+      graphData: {
+        data: {
+          labels: dataLabels,
+          datasets: [
+            {
+              label: "# of Times Mood Was Selected",
+              data: numericalData,
+              fill: true,
+              backgroundColor: this.addGraphGradient(),
+              borderColor: "black",
+              borderWidth: 1,
+              pointBackgroundColor: "#7FBEF9",
+              pointBorderColor: "#7FBEF9",
+              pointRadius: 5,
+            },
+          ],
+        },
         options: options,
       },
     });
@@ -156,8 +203,8 @@ export default class Graph extends Component {
             </div>
             <Line
               id="lineChart"
-              data={this.state.chartData.data}
-              options={this.state.chartData.options}
+              data={this.state.graphData.data}
+              options={this.state.graphData.options}
             />
           </div>
         </div>
