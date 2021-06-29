13f98b30fe899de73a57c5f6d572ae13502922b0
Lindsay Esterman <lindsayesterman@gmail.com>
2021-06-29 14:56:46 -0400

    off-by-one fix:CB

--- src/Graph/Graph.js
+++ src/Graph/Graph.js
@@ -26,8 +26,16 @@ export default class Graph extends Component {
     this.setState({
       timeRange: e.target.value,
     });
-    console.log("state" + this.state.timeRange);
-    this.getGraphData();
+    this.setState({ timeRange: e.target.value}, this.getGraphData);
+
+    // this.setState({
+    //   timeRange: {
+    //     ...this.state.timeRange,
+    //     timeRange: e.target.value,
+    //   },
+    // });
+    console.log(this.state.timeRange)
+    // this.getGraphData();
   };
 
   addGraphGradient = () => {
@@ -52,7 +60,7 @@ export default class Graph extends Component {
         ],
         datasets: {
           data: [1, 3, 3, 4, 3, 5, 4],
-        }
+        },
       };
       return { data };
     } else if (this.state.timeRange === "month") {
@@ -62,8 +70,11 @@ export default class Graph extends Component {
           21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
         ],
         datasets: {
-          data: [1,4,5,1,2,3,2,3,4,4,3,2,4,5,4,5,4,2,3,2,3,5,3,2,5,3,3,3,4,5,5,5],
-        }
+          data: [
+            1, 4, 5, 1, 2, 3, 2, 3, 4, 4, 3, 2, 4, 5, 4, 5, 4, 2, 3, 2, 3, 5, 3,
+            2, 5, 3, 3, 3, 4, 5, 5, 5,
+          ],
+        },
       };
       return { data };
     } else if (this.state.timeRange === "year") {
@@ -84,7 +95,7 @@ export default class Graph extends Component {
         ],
         datasets: {
           data: [2.7, 3, 3.2, 3, 3.6, 4, 3.5, 4, 4.5, 3.9, 4, 3.8],
-        }
+        },
       };
       return { data };
     }
@@ -92,11 +103,17 @@ export default class Graph extends Component {
 
   getGraphData = () => {
     const dataLabels = this.whichData().data.labels;
-    const numericalData = this.whichData().data.datasets.data
+    const numericalData = this.whichData().data.datasets.data;
 
     const options = {
-      plugins: { legend: { display: false } },
-      layout: { padding: { bottom: 10 } },
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip:{
+          enabled:true
+        }
+      },
       scales: {
         y: {
           ticks: {
